cf8cad90c7da00283d422c05f9fc0b1e56e835e7
EC2 Default User <126648712+taka0705@users.noreply.github.com>
2023-03-23 08:24:06 +0000

    [Finish]応用課題2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
 
+  validates :introduction,length: {maximum: 50 }
+
 
 
   def get_profile_image
