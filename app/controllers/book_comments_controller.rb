5201fb19cd1831ca52eebcea0daf9886b80bae74
EC2 Default User <126648712+taka0705@users.noreply.github.com>
2023-04-05 08:16:24 +0000

    [Finish]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,21 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
     # comment = PostComment.new(post_comment_params)
     # comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book.id)
+    @comment.book_id = @book.id
+    @comment.save
+    # redirect_to book_path(book.id)
+    render :book_comments
   end
 
   def destroy
-    
+    @book = Book.find(params[:book_id])
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    # redirect_to book_path(params[:book_id])
+    render :book_comments
   end
 
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,23 +1,27 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     # favorite = current_user.favorites.new(book_id: book.id)
     # カリキュラムを参照しながらだと↑の書き方だけど↓の方がわかりやすい
-    favorite = Favorite.new(book_id: book.id,user_id: current_user.id)
+    favorite = Favorite.new(book_id: @book.id,user_id: current_user.id)
     #book_idはカラム名、book.idはBook.find(params[:book_id]).idと同じ意味
     favorite.save
-    redirect_back(fallback_location: root_path)
-    # redirect_to book_path(book)
-    #ここのredirect_toを書き換える必要がある。
-    # 同じページに移るようにするという書き方を調べる。
+    # redirect_back(fallback_location: root_path)
+
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
 end
+
+# リダイレクト先を削除したことにより、
+# リダイレクト先がない、かつJavaScriptリクエストという状況になり、
+
+# createアクション実行後は、create.js.erbファイルを、
+# destroyアクション実行後はdestroy.js.erbファイルを探すようになります
