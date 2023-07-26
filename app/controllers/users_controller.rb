8926b85c52b602c8a1b3b438f34c88bc7deae4a6
EC2 Default User <126648712+taka0705@users.noreply.github.com>
2023-07-26 14:33:27 +0000

    コメントアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
     # 現在ログインしているユーザ=current_user
     @following_users = @user.following_user
     @follower_users = @user.follower_user
+    # この上二つを「そのユーザーがフォローした数とフォローされた数」に変えないとユーザー一覧でその数を引っ張ってこれないはず。
     @users = User.all
     @book = Book.new
   end
