348625ed2a3e98544480493bfe4af6d0078fa3fd
EC2 Default User <126648712+taka0705@users.noreply.github.com>
2023-10-09 13:44:37 +0000

    コメントアウト

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,10 +3,9 @@ class Book < ApplicationRecord
   has_many :favorites,dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :week_favorites,->{where(created_at: 1.week.ago.biginning_of_day..Time.current.end_of_day)}
-  
-  #↓閲覧数表示のためのアソシエーション
+
   has_many :read_counts, dependent: :destroy
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
